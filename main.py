669101ce478bba77e1dbf1c6ad52bf3f6e2706e3
leelakuamr <eedupugantil@gmail.com>
2026-02-15 21:14:12 +0530

    latest code

--- main.py
+++ main.py
@@ -33,8 +33,9 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8080", "http://127.0.0.1:8080",
         "http://localhost:5500", "http://127.0.0.1:5500",
+        "https://aicodere.web.app",
     ],
-    allow_origin_regex=r"https://.*\.netlify\.app",
+    allow_origin_regex=r"https://.*\.(netlify\.app|web\.app)",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
